412d1df896fad23f2b69b7a3cbfbe7a9138267cc
Bashaar Ahmed <bashaarahmedchohan@gmail.com>
2026-03-20 06:55:03 +0500

    Update landing title to Bashaar Ahmed

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              BASHAAR
-              <br />
-              <span>AHMED</span>
+              Bashaar Ahmed
             </h1>
           </div>
           <div className="landing-info">
