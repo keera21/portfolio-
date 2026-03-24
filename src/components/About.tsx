6c83aab88ec1f33765603cb69d80a8ad738b84f4
Bashaar Ahmed <bashaarahmedchohan@gmail.com>
2026-03-24 21:25:59 +0500

    Fix text alignment for Proficient in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,10 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Passionate Mechatronics Engineering student with hands-on experience in robotics, embedded systems, and autonomous systems. Experienced in designing and building multi-motor rovers, robotic arms, and high-torque platforms. Proficient in Python, C/C++, ROS2, SLAM, and computer vision, with strong skills in AI-based robotic simulations and control systems.
+          Passionate Mechatronics Engineering student with hands-on experience in robotics, embedded systems, and autonomous systems. Experienced in designing and building multi-motor rovers, robotic arms, and high-torque platforms.
+          <br />
+          <br />
+          Proficient in Python, C/C++, ROS2, SLAM, and computer vision, with strong skills in AI-based robotic simulations and control systems.
         </p>
       </div>
     </div>
